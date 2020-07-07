91146436f400c18ffff6516b406962fb659d3d31
zhaoshuai <9825999220@qq.com>
2020-07-07 23:31:35 +0800

    程序初调成功

---
+++ Tidal/lstmTidal.py
@@ -0,0 +1,126 @@
+
+"""
+高阶内容  5.12 RNN LSTM循环神经网络（分类例子）
+This code is a modified version of the code from this link:
+https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/recurrent_network.py
+His code is a very good one for RNN beginners. Feel free to check it out.
+"""
+import pandas as pd
+import numpy as np
+import Tidal.DataSet
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+# set random seed for comparing the two result calculations
+tf.set_random_seed(1)
+
+# this is data
+#mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+#第一行数据读不到？？？所以在第一行数据补了一行填充数据
+df = pd.read_csv('C:\\Users\\98259\\Desktop\\6.9学习相关文档\样本数据\\fiftMin\\samplePeakHour_训练数据 - 副本Line.csv')
+data_sample = np.array(df).astype(float)
+#print(data_sample)
+
+df = pd.read_csv('C:\\Users\\98259\\Desktop\\6.9学习相关文档\样本数据\\fiftMin\\label_2.csv')
+data_label = np.array(df).astype(float)
+
+
+# hyperparameters
+lr = 0.001
+training_iters = 10000
+batch_size = 64 #batch_size 会影响RNN函数输出？？？？？？？？？？
+
+n_inputs = 2   # 两列数据，分别代表早晚高峰 2*16
+n_steps = 16   # time steps
+n_hidden_units = 128   # neurons in hidden layer
+n_classes = 2      # 二分类问题
+
+# tf Graph input
+x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+y = tf.placeholder(tf.float32, [None, n_classes])
+
+# Define weights
+weights = {
+    # (2, 128)
+    'in': tf.Variable(tf.random_normal([n_inputs, n_hidden_units])),
+    # (128, 2)
+    'out': tf.Variable(tf.random_normal([n_hidden_units, n_classes]))
+}
+biases = {
+    # (128, )
+    'in': tf.Variable(tf.constant(0.1, shape=[n_hidden_units, ])),
+    # (2, )
+    'out': tf.Variable(tf.constant(0.1, shape=[n_classes, ]))
+}
+
+
+def RNN(X, weights, biases):
+    # hidden layer for input to cell
+    ########################################
+
+    # transpose the inputs shape from
+    # X ==> (128 batch * 28 steps, 28 inputs)
+    X = tf.reshape(X, [-1, n_inputs])
+
+    # into hidden
+    # X_in = (128 batch * 28 steps, 128 hidden)
+    X_in = tf.matmul(X, weights['in']) + biases['in']
+    # X_in ==> (128 batch, 28 steps, 128 hidden)
+    X_in = tf.reshape(X_in, [-1, n_steps, n_hidden_units])
+
+    # cell
+    ##########################################
+
+    # basic LSTM Cell.
+    if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+        cell = tf.contrib.rnn.BasicLSTMCell(n_hidden_units)
+    else:
+        cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units, forget_bias=1.0, state_is_tuple=True)
+    # lstm cell is divided into two parts (c_state, h_state)
+    init_state = cell.zero_state(batch_size, dtype=tf.float32)
+
+    outputs, final_state = tf.nn.dynamic_rnn(cell, X_in, initial_state=init_state, time_major=False)
+
+    # unpack to list [(batch, outputs)..] * steps
+    if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+        outputs = tf.unpack(tf.transpose(outputs, [1, 0, 2]))    # states is the last outputs
+    else:
+        outputs = tf.unstack(tf.transpose(outputs, [1,0,2]))
+    results = tf.matmul(outputs[-1], weights['out']) + biases['out']    # shape = (128, 10)
+    return results
+
+
+pred = RNN(x, weights, biases)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+train_op = tf.train.AdamOptimizer(lr).minimize(cost)
+
+correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+with tf.Session() as sess:
+    # tf.initialize_all_variables() no long valid from
+    # 2017-03-02 if using tensorflow >= 0.12
+    if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
+        init = tf.initialize_all_variables()
+    else:
+        init = tf.global_variables_initializer()
+    sess.run(init)
+    step = 0
+    td = Tidal.DataSet.DataSet(data_sample, data_label, 104)
+    while step * batch_size < training_iters:
+        #batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        batch_xs, batch_ys = td.next_batch(batch_size)
+        batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs])
+        #batch_ys = batch_ys.reshape([n_steps,n_inputs])#待考究！！！！！！！！！！！
+        sess.run([train_op], feed_dict={
+            x: batch_xs,
+            y: batch_ys,
+        })
+        if step % 20 == 0:
+            print(sess.run(accuracy, feed_dict={
+            x: batch_xs,
+            y: batch_ys,
+            }))
+        step += 1
--- test.py
+++ test.py
@@ -2,73 +2,27 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import csv
 
-def createGraphByRMid(filePath):
-    g = nx.DiGraph()  # 创建空的有向图
-    preAdjDict = {} #存储了每个节点的前驱节点，每个key(node)对应一个list，解决有向图中邻接点不保存前驱节点的问题
+if __name__ == '__main__':
+    g = nx.Graph()  # 创建空的无向图
+
+    filePath = 'C:\\Users\\98259\\Desktop\\6.9学习相关文档\样本数据\\fiftMin\\sample - 副本.csv'
+    count = 0
     with open(filePath, 'r') as file:
         reader = csv.reader(file)
         for r in reader:
-            # if(judgeRoadAttr(r[2])):
-            #     print(r[0]+","+r[2])
-            #     continue
-            g.add_node(r[5])
-            g.add_node(r[6])
-            eNode = None
-            sNode = None
-            if(r[4] == '2'):
-                g.add_edge(r[5], r[6], linkID=r[0], kindNum=r[1], kind=r[2], width=r[3], dir=r[4], len=r[7], laneNum=r[8])
-                eNode = r[6]
-                sNode = r[5]
-            else:
-                g.add_edge(r[6], r[5], linkID=r[0], kindNum=r[1], kind=r[2], width=r[3], dir=r[4], len=r[7], laneNum=r[8])
-                eNode = r[5]
-                sNode = r[6]
-            if (eNode in preAdjDict.keys()):
-                list = preAdjDict[eNode]
-                list.append(sNode)
-                preAdjDict[eNode] = list
-            else:
-                list = [sNode]
-                preAdjDict[eNode] = list
-    return g, preAdjDict
+            for i in range(63):
+                g.add_edge(r[i], r[i+1])
 
-#判断道路种别代码，是否含有我们不需要的link类型，如果是，返回true，否则返回false
-def judgeRoadAttr(kind):
-    otherKindList = ['0a','0b','0e','01','03','05','13','11']
-    kindList = kind.split('|')
-    for item in kindList:
-        if(item[:2] == '00'):
-            return True
-        if(item[2:] in otherKindList):
-            return True
-    return False
+            count += 1
+            if(count == 2):
+                print(count)
+                break
 
-def reversalGraph(g, preAdjDict):
-    revG = nx.DiGraph()
-    nodes = g.nodes()
-    for node in nodes:
-        adjNodeDict = g[node]  # 所有邻接点
-        for key in adjNodeDict:
-            edge = adjNodeDict[key]
-            revG.add_node(edge['linkID'], kindNum=edge['kindNum'], kind=edge['kind'], width=edge['width'],
-                          dir=edge['dir'], len=edge['len'], laneNum=edge['laneNum'])
-            #有一些node可能没有前驱
-            if(node in preAdjDict.keys()):
-                for preNode in preAdjDict[node]:
-                    revG.add_edge(g[preNode][node]['linkID'], edge['linkID'])
-    return revG
+    #可视化展示
+    nx.draw(g)
+    plt.show()
 
 
-if __name__ == '__main__':
-    #filePath = 'D:\\program\\congestion\\test.csv'
-    filePath = 'D:\\program\\congestion\\originMapWithOutBiDirec.csv'
-    g, preAdjDict = createGraphByRMid(filePath)
-    print(g['531572'])
-    print(preAdjDict['531574'])
-    print(g['531572']['531574'])
-    revG = reversalGraph(g, preAdjDict) #得到以边为节点的有向图
-    print(revG['19468718'])
-    print(revG.nodes['19468716'])
     #
     # # 1. 创建文件对象
     # f = open('D:\\program\\congestion\\intersectionLink.csv', 'w', encoding='utf-8', newline ='')
